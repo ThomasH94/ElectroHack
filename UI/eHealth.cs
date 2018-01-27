af5235c58ede62a3cefaaf5d9ecfdc8271edac70
jv7272 <srry72@gmail.com>
2018-01-26 22:04:22 -0700

    Add files via upload

---
+++ UI/eHealth.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eHealth : MonoBehaviour 
+{
+	[SerializeField]
+	private int health;
+	[SerializeField]
+	private Renderer[] healthBar;
+
+	// Use this for initialization
+	void Start () 
+	{
+		healthBar = GetComponentsInChildren<Renderer>();
+		health = 3;
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			health++;
+			barShow ();
+		}
+		if (Input.GetKeyUp(KeyCode.DownArrow)) 
+		{
+			health--;
+			barShow ();
+		}
+	}
+	void barShow()
+	{
+		switch (health) 
+		{
+		case 1:
+			healthBar [1].enabled = false;
+			healthBar [2].enabled = false;
+			healthBar [3].enabled = true;
+			print (1);
+			break;
+		case 2:
+			healthBar [1].enabled = false;
+			healthBar [2].enabled = true;
+			healthBar [3].enabled = false;
+			print (2);
+			break;
+		case 3:
+			healthBar [1].enabled = true;
+			healthBar [2].enabled = false;
+			healthBar [3].enabled = false;
+			print (3);
+			break;
+		case 0:
+			healthBar [1].enabled = false;
+			healthBar [2].enabled = false;
+			healthBar [3].enabled = false;
+			print (4);
+			break;
+		}
+	}
+}
---
+++ UI/spawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnEnemy : MonoBehaviour 
+{
+	public GameObject e;
+	public GameObject[] spawn;
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.Alpha1)) 
+		{
+			Instantiate (e, new Vector3(spawn[0].transform.position.x,spawn[0].transform.position.y,spawn[0].transform.position.z),Quaternion.identity);
+			print(spawn[0].name);
+		}	
+		if (Input.GetKeyUp (KeyCode.Alpha2)) 
+		{
+			Instantiate (e, new Vector3(spawn[1].transform.position.x,spawn[1].transform.position.y,spawn[1].transform.position.z),Quaternion.identity);
+			print(spawn[1].name);
+		}
+		if (Input.GetKeyUp (KeyCode.Alpha3)) 
+		{
+			Instantiate (e, new Vector3(spawn[2].transform.position.x,spawn[2].transform.position.y,spawn[2].transform.position.z),Quaternion.identity);
+			print(spawn[2].name);
+		}
+	}
+}
