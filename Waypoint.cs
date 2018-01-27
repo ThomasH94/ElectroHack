1177f8ae370b98658b3eb150cca0f8ea4c5536a1
Thanatos891 <35860098+Thanatos891@users.noreply.github.com>
2018-01-27 07:00:03 -0700

    Add files via upload
    Started Building a Waypoint System for the bot to follow.

---
+++ Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+	public List<Waypoint> waypoints;
+	public bool loopPath = true;
+	public bool reversePath = false;
+	public float pauseTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
